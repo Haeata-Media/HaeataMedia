b70accc9cf6df9881f560cf563e6bdb5d720eb80
brooklyn-lyndontonga <brooklynlt24@gmail.com>
2025-01-25 22:57:22 +1300

    added home, about component content

---
+++ client/components/About.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Haeata Media</h1>
+      <h2>Web development for Māori, by Māori.</h2>
+
+      <p>Haeata Media was founded with a vision: to provide culturally authentic web design solutions that empower businesses and communities. 
+        Based in Rural Gisborne, Aotearoa, we work with clients to create websites that combine technical excellence with Māori cultural elements such as traditional patterns, te reo Māori, and storytelling.
+
+We believe in creating meaningful digital experiences that connect people to our common values while driving your business forward.
+Our mission is to help you shine online—authentically in your Mana Māori Motuhake.</p>
+
+<h2>Meet The Heart Behind Haeata Media</h2>
+
+<p>Tēnā koutou katoa,
+  I te taha Whatukura he uri nā Hineamaru i ngā rekereke o Ngati Hine.
+  I te taha Mareikura he uri nā Porourangi i ngā akau o Ngati Porou, i te Tairawhiti.
+  Ko Brooklyn Lyndon-Tonga ahau. </p>
+  
+  <p>Kia ora,I’m Brooklyn Lyndon-Tonga, the founder, web developer, and creative force behind Haeata Media.
+    As a proud Māori and passionate advocate for all things Māori, I created this business to help people like myself build meaningful online spaces that celebrate our values and identity.
+
+While I’m a one-person operation, I’m far from working alone. I have a trusted network of designers, content creators, and tech specialists I can call on to bring our vision to life.
+Whether it’s crafting beautiful designs, writing compelling content, or tackling technical challenges, I ensure that our project has all that it needs.
+
+At Haeata Media, I focus on weaving together storytelling, te ao Māori, and innovative web design to create websites that feel personal and powerful.
+Let’s work together to build something unique, authentic, and meaningful.</p>
+
+<button></button>
+
+    </div>
+  )
+}
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,7 +4,13 @@ function Home() {
   return (
     <div>
       <h1>Haeata Media</h1>
-      <h2></h2>
+      <h2>Where culture meets digital experience</h2>
+      <p>Nau mai ki Haeata Media, where we create websites that honor your story and reflect your values. As a Māori-owned web development firm, we specialize in helping you bring to life culturally rich and user-friendly platforms for businesses, non-profits, and entrepreneurs.</p>
+      <h3>Let’s work together to create a digital space that speaks to your audience and celebrates your identity.</h3>
+
+      <button>Start Your Journey Today</button>
     </div>
   )
 }
+
+export default Home
